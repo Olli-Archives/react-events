36e34466d515063dfa4e71d2c8678c1eaf6650a2
ollinieminen <otn987@gmail.com>
2019-05-14 10:20:41 -0700

    add dyncamic color and post css

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,24 +1,24 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, fragment } from 'react';
 import PropTypes from 'prop-types';
+import styles from './ColorPicker.css';
 
 export default class ColorPicker extends PureComponent {
   static propTypes = {
     color: PropTypes.string.isRequired
   }
 
-
+  
   clickHandler =  ()=>{
     console.log('you clicked', this.props.color);
   }
-
+  
   render(){
+    const { color } = this.props;
+
     return (
-      <>
-      <button className={this.props.color} onClick={this.clickHandler} style={
-        {  width: '300px', height:'100px' }
-      }>{this.props.color}</button>
-      
-      </>
+      <section className={styles.ColorPicker}>
+        <button className={styles[color]} onClick={this.clickHandler} >{this.props.color}</button> 
+      </section>
     );
   }
 }
